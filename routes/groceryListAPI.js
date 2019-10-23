c8fa963ddb111c385c02bdfa814e1cf81f9800a4
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2019-10-23 15:33:35 -0500

    Add groceryListAPI.

--- routes/groceryListAPI.js
+++ routes/groceryListAPI.js
@@ -1,37 +1,36 @@
 const router = require("express").Router();
-const Meal = require("../models/Meal");
+const GroceryListItem = require("../models/GroceryListItem");
 
-router.get("/api/meals/:userID", (req, res) => {
-    let userID = req.params.userID;
-    Meal.find({ user: userID })
+router.get("/api/groceryList/:userID", (req, res) => {
+    GroceryListItem.find({ user: userID })
         .then(data => res.json(data))
         .catch(err => console.log(err));
 });
 
-router.post("/api/meals", (req, res) => {
+router.post("/api/groceryList", (req, res) => {
     console.log("POST route hit.");
-    let newMeal = new Meal(req.body.data);
-    newMeal
+    let listItem = new GroceryListItem(req.body.data);
+    listItem
         .save()
         .then(data => console.log(data))
         .catch(err => console.log(err));
     res.end();
 });
 
-router.put("/api/meals/:id", (req, res) => {
+router.put("/api/groceryList/:id", (req, res) => {
     console.log("PUT route hit.");
-    let mealID = req.params.id;
-    let mealUpdate = req.body.data;
-    Meal.findByIdAndUpdate(mealID, mealUpdate, { new: true, useFindAndModify: false })
+    let itemID = req.params.id;
+    let itemUpdate = req.body.data;
+    GroceryListItem.findByIdAndUpdate(itemID, itemUpdate, { new: true, useFindAndModify: false })
         .then(data => res.json(data))
         .catch(err => console.log(err));
 });
 
-router.delete("/api/meals/:id", (req, res) => {
+router.delete("/api/groceryList/:id", (req, res) => {
     console.log("DELETE route hit.");
-    let mealID = req.params.id;
-    console.log(mealID);
-    Meal.findByIdAndDelete(mealID)
+    let itemID = req.params.id;
+    console.log(itemID);
+    GroceryListItem.findByIdAndDelete(itemID)
         .then(data => {
             console.log(data);
             res.json({ successMessage: "Sucessfully deleted." });
