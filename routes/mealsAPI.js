20e3aa16b034b478947834c151b0eb56f3c40421
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2019-10-23 14:18:49 -0500

    Add CRUD routes for mealsAPI, more API testing.

--- routes/mealsAPI.js
+++ routes/mealsAPI.js
@@ -1,11 +1,47 @@
 const router = require("express").Router();
 const Meal = require("../models/Meal");
 
-router.get("/meals/:userID", (req, res) => {
+router.get("/api/meals/:userID", (req, res) => {
     let userID = req.params.userID;
     Meal.find({ user: userID })
         .then(data => res.json(data))
         .catch(err => console.log(err));
+});
+
+router.post("/api/meals", (req, res) => {
+    console.log("POST route hit.");
+    let newMeal = new Meal(req.body.data);
+    newMeal
+        .save()
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    res.end();
+});
+
+router.put("/api/meals/:id", (req, res) => {
+    console.log("PUT route hit.");
+    let mealID = req.params.id;
+    let mealUpdate = req.body.data;
+    Meal.findByIdAndUpdate(mealID, mealUpdate, { new: true, useFindAndModify: false })
+        .then(data => res.json(data))
+        .catch(err => console.log(err));
+});
+
+router.delete("/api/meals/:id", (req, res) => {
+    console.log("DELETE route hit.");
+    let mealID = req.params.id;
+    console.log(mealID);
+    Meal.findByIdAndDelete(mealID)
+        .then(data => {
+            console.log(data);
+            res.send("Meal deleted.");
+        })
+        .catch(error => {
+            if (error) {
+                console.log(error);
+                res.send("Deletion not successful.");
+            }
+        });
 });
 
 module.exports = router;
