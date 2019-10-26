d751cc55b5945cb012da85e90bc8d1c1c223af11
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2019-10-25 23:04:58 -0500

    Home view cards and buttons.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,9 +2,9 @@
     <div class="home">
         <header class="site-header">
             <i class="fas fa-bars fa-2x mobile-navtoggle"></i>
-            <h1 class="site-header--title">
+            <h1 class="text-5xl text-orange-400">
                 Grocery
-                <i class="fas fa-shopping-cart" style="color: orange; font-size: 1.5rem;"></i> Helper
+                <i class="fas fa-shopping-cart text-2xl text-orange-400"></i> Helper
             </h1>
             <section>
                 <i class="far fa-user-circle fa-2x" @click="showUserMenu = !showUserMenu"></i>
@@ -18,16 +18,32 @@
                 </ul>
             </section>
         </header>
-        <p class="text-red-600">TEST</p>
-        <div class="main-content-wrapper">
-            <section class="home--module-row-first">
-                <div class="home--module-card"></div>
-                <div class="home--module-card"></div>
-            </section>
-            <section class="home--module-row-second">
-                <div class="home--module-card"></div>
-            </section>
-        </div>
+        <section class="w-11/12 m-auto main-content-container">
+            <div class="h-64 w-auto" style="background-color: rgba(211,211,211, .4);">
+                <div class="h-48"></div>
+                <button
+                    class="bg-orange-400 rounded-full p-3 font-bold my-1 hover:bg-orange-300 w-40 text-sm"
+                >Manage Meals</button>
+            </div>
+            <div class="h-64 w-auto" style="background-color: rgba(211,211,211, .4);">
+                <div class="h-48"></div>
+                <button
+                    class="bg-orange-400 rounded-full p-3 font-bold my-1 hover:bg-orange-300 w-40 text-sm"
+                >Grocery List</button>
+            </div>
+            <div class="h-64 w-auto" style="background-color: rgba(211,211,211, .4);">
+                <div class="h-48"></div>
+                <button
+                    class="bg-orange-400 rounded-full p-3 font-bold my-1 hover:bg-orange-300 w-40 text-sm"
+                >Meal Plan</button>
+            </div>
+            <div class="h-64 w-auto" style="background-color: rgba(211,211,211, .4);">
+                <div class="h-48"></div>
+                <button
+                    class="bg-orange-400 rounded-full p-3 font-bold my-1 hover:bg-orange-300 w-40 text-sm"
+                >Seasonal Veggies</button>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -67,11 +83,6 @@ export default {
     visibility: hidden;
 }
 
-.site-header--title {
-    font-size: 3rem;
-    color: orange;
-}
-
 .far.fa-user-circle {
     cursor: pointer;
     margin-right: 25px;
@@ -82,27 +93,9 @@ export default {
     right: 20px;
 }
 
-.home--module-row-first {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    max-width: 1100px;
-    margin: 0 auto 40px auto;
-}
-
-.home--module-row-second {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    max-width: 1100px;
-    margin: 0 auto;
-}
-
-.home--module-card {
-    width: 31.25rem;
-    min-height: 15rem;
-    border: 2px solid lightgray;
-    border-radius: 4px;
+.main-content-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+    grid-gap: 1rem;
 }
 </style>
